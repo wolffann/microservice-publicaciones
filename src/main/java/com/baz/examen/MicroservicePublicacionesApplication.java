9c3d4a292a812cd12ee48d9193b9b187993bd38f
enrique728811 <enrique728811@gmail.com>
2022-07-13 23:38:01 -0500

    Se sube carga inicial

---
+++ src/main/java/com/baz/examen/MicroservicePublicacionesApplication.java
@@ -0,0 +1,13 @@
+package com.baz.examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicePublicacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicePublicacionesApplication.class, args);
+	}
+
+}
